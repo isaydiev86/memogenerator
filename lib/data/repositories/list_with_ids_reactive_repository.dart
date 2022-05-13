8ebbf5baad40affb9212e660e9232993bbb9453f
ildansaydiev <isaydiev86@gmail.com>
2022-05-13 23:54:31 +0500

    hometask_4 fix

--- lib/data/repositories/list_with_ids_reactive_repository.dart
+++ lib/data/repositories/list_with_ids_reactive_repository.dart
@@ -1,47 +1,9 @@
-import 'package:rxdart/rxdart.dart';
+import 'package:memogenerator/data/repositories/list_reactive_repository.dart';
 import 'package:collection/collection.dart';
 
-abstract class ListWithIdsReactiveRepository<T> {
-  final updater = PublishSubject<Null>();
-
-  Future<List<String>> getRawData();
-
-  Future<bool> saveRawData(final List<String> items);
-
-  T convertFromString(final String rawItem);
-
-  String convertToString(final T item);
-
+abstract class ListWithIdsReactiveRepository<T>
+    extends ListReactiveRepository<T> {
   dynamic getId(final T item);
-
-  Future<List<T>> getItems() async {
-    final rawItems = await getRawData();
-    return rawItems.map((rawItem) => convertFromString(rawItem)).toList();
-  }
-
-  Future<bool> setItems(final List<T> items) async {
-    final rawItems = items.map((item) => convertToString(item)).toList();
-    return _setRawItems(rawItems);
-  }
-
-  Stream<List<T>> observeItems() async* {
-    yield await getItems();
-    await for (final _ in updater) {
-      yield await getItems(); //добавление нового зн-ия
-    }
-  }
-
-  Future<bool> addItem(final T item) async {
-    final items = await getItems();
-    items.add(item);
-    return setItems(items);
-  }
-
-  Future<bool> removeItem(final T item) async {
-    final items = await getItems();
-    items.remove(item);
-    return setItems(items);
-  }
 
   Future<bool> addItemsOrReplaceById(final T newItem) async {
     final items = await getItems();
@@ -63,10 +25,5 @@ abstract class ListWithIdsReactiveRepository<T> {
   Future<T?> getItemById(final dynamic id) async {
     final items = await getItems();
     return items.firstWhereOrNull((item) => getId(item) == id);
-  }
-
-  Future<bool> _setRawItems(final List<String> rawItems) async {
-    updater.add(null); //уведомление о новом зн-ии
-    return saveRawData(rawItems);
   }
 }
